a45582d1e37e83fa482ad6e86c6856ad55451ecf
Victor Berco <vberco.76@gmail.com>
2018-12-06 17:38:56 +0200

    issueMainPage generate password

--- proiect_pass_storage/MainWindow.xaml.cs
+++ proiect_pass_storage/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Text.RegularExpressions;
+using System;
 
 namespace proiect_pass_storage {
     /// <summary>
@@ -19,16 +8,109 @@ namespace proiect_pass_storage {
     /// </summary>
     public partial class MainWindow : Window {
         private UserData data;
-
+        private const int PASSWORD_MIN_LENGTH = 8;
+        private const int PASSWORD_MAX_LENGTH = 30;
+        private const int NON_ALFANUMERIC_CHARACTER_LENGTH = 2;
         public MainWindow() {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
         public MainWindow(UserData data) {
             InitializeComponent();
             this.data = data;
             buttonCreateResourse.Click += ButtonCreateResourse_Click;
+            buttonSearchPassword.Click += ButtonSearchPassword_Click;
+            buttonGeneratePassword.Click += ButtonGeneratePassword_Click;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonGeneratePassword_Click(object sender, RoutedEventArgs e) {
+            var userPasswordLength = textBoxPasswordLength.Text;
+            int passwordLength = PASSWORD_MAX_LENGTH;
+
+            if (!userPasswordLength.Equals("")) {
+                int length;
+                if (int.TryParse(userPasswordLength, out length)) {
+                    if (length >= PASSWORD_MIN_LENGTH && length <= PASSWORD_MAX_LENGTH) {
+                        passwordLength = length;
+                    }
+                }
+            }       
+               
+            int nonAlfaNumericLength = NON_ALFANUMERIC_CHARACTER_LENGTH;
+            var userNonAlfaNumericLength = textBoxNrSpecialSymbols.Text;
+            if (!userNonAlfaNumericLength.Equals("")) {
+                int length;
+                if (int.TryParse(userNonAlfaNumericLength, out length)) {
+                    if (passwordLength - length > 3) {
+                        nonAlfaNumericLength = length;
+                    }
+                }
+            }
+            var generatedPassword = generatePassword(passwordLength, nonAlfaNumericLength);
+            textBoxInputPassword.Text = generatePassword(passwordLength, nonAlfaNumericLength);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="passwordLength"></param>
+        /// <param name="nonAlfaNumericLength"></param>
+        /// <returns></returns>
+        private string generatePassword(int passwordLength, int nonAlfaNumericLength) {
+            string password = "";
+            string regexString = @"^(?=(.*[^A-Za-z0-9]){2})(?=(.*[A-Z]){2})(?=(.*[a-z]){2})(?=(.*\d){" + nonAlfaNumericLength + "}).+";
+            var regex = new Regex(regexString);
+            do {
+                password = getRandomPassword(passwordLength);
+            } while (!regex.IsMatch(password));
+
+            return password;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private string getRandomPassword(int length) {
+            string password = "";
+            var random = new Random((int)DateTime.Now.Ticks);
+            try {
+                byte[] result = new byte[length];
+                for (int index = 0; index < length; index++) {
+                    result[index] = (byte)random.Next(33, 126);
+                }
+                password = System.Text.Encoding.ASCII.GetString(result);
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message, ex);
+            }
+
+            return password;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSearchPassword_Click(object sender, RoutedEventArgs e) {
+            gridCreatePassword.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ButtonCreateResourse_Click(object sender, RoutedEventArgs e) {
             gridCreatePassword.Visibility = Visibility.Visible;
         }
